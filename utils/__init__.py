cc86a94f588fdc43e31ffbe7347e6ba2e821825d
Natachat Teecharcharn <techazuza@gmail.com>
2025-10-20 12:31:26 +0700

    Remove unused parsers in parse_utils, also refactor

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,1 @@
 from .file_utils import read_csv
-from .parse_utils import parse_path, parse_path_feature, parse_path_feature_2
--- utils/parse_utils.py
+++ utils/parse_utils.py
@@ -1,50 +1,11 @@
-import argparse
-
-
-def parse_path():
-    parser = argparse.ArgumentParser(
-        prog="describe.py",
-        description="Describe numerical columns' statistics",
-    )
-    parser.add_argument("path", help=".csv file to describe")
-    args = parser.parse_args()
-    return args
-
-
-def parse_path_feature():
-    parser = argparse.ArgumentParser(
-        prog="histogram.py",
-        description="Plot Histogram",
-        add_help=False
-    )
-    parser.add_argument("-h", action="store_true", help="Help message")
-    parser.add_argument("path", help=".csv file to describe")
-    parser.add_argument("feature", help="Feature name")
-    args = parser.parse_args()
-    return args
-
-
-def parse_path_feature_2():
-    parser = argparse.ArgumentParser(
-        prog="histogram.py",
-        description="Plot Histogram",
-        add_help=False
-    )
-    parser.add_argument("-h", action="store_true", help="Help message")
-    parser.add_argument("path", help=".csv file to describe")
-    parser.add_argument("feature_x", help="Feature X name")
-    parser.add_argument("feature_y", help="Feature Y name")
-    args = parser.parse_args()
-    return args
+COLUMN_NAMES = [
+    "Arithmancy", "Astronomy", "Herbology", "Defense Against the Dark Arts", "Divination",
+    "Muggle Studies", "Ancient Runes", "History of Magic", "Transfiguration", "Potions",
+    "Care of Magical Creatures", "Charms", "Flying"
+]
 
 
 def get_description():
-    COLUMN_NAMES = [
-        "Arithmancy", "Astronomy", "Herbology", "Defense Against the Dark Arts", "Divination",
-        "Muggle Studies", "Ancient Runes", "History of Magic", "Transfiguration", "Potions",
-        "Care of Magical Creatures", "Charms", "Flying"
-    ]
-    
     des = "Draw a scatter plot for the selected columns:\n"
     for c in COLUMN_NAMES:
         des += f"  {c}\n"
